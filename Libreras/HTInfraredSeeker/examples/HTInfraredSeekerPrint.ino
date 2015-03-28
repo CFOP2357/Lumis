e85f68f40a196bd908b4f6317bdeb88bc4f1aef4
xaviarazor <prietojavi551@gmail.com>
2015-03-27 22:35:07 -0600

    Create HTInfraredSeekerPrint.ino

---
+++ Libreras/HTInfraredSeeker/examples/HTInfraredSeekerPrint.ino
@@ -0,0 +1,14 @@
+#include <Wire.h>
+#include <HTInfraredSeeker.h>
+InfraredSeeker seeker=InfraredSeeker();
+InfraredInput seekerInput;//data structure with the seeker's readings
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+}
+void loop() {
+  seekerInput=seeker.ReadAC();
+  Serial.print("Strength     Direction");
+  Serial.print(seekerInput.Strength+"  "); 
+  Serial.println(seekerInput.Direction);
+}
