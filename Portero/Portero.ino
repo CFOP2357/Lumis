b2e071a753657508bef94c9552a94f30eb33ee28
Yoguel Salazar <yoguel.salazar@gmail.com>
2015-04-15 18:57:58 -0500

    prueba 4

--- Portero/Portero.ino
+++ Portero/Portero.ino
@@ -4,6 +4,8 @@
 #include <Wire.h>
 #include <HTInfraredSeeker.h>
 
+#define SPEED                   150
+
 
 Movimientos robot=  Movimientos();
 
@@ -11,8 +13,10 @@ CompassSensor compass=CompassSensor();
 
 InfraredSeeker seeker=InfraredSeeker();
 InfraredInput seekerInput;
+
 bool oriented=false;
-bool closeB=false;
+bool black=true;
+
 int startingAngle;
 
 void setup() {
@@ -21,6 +25,8 @@ void setup() {
   
   compass.setAddress(0x21);
   startingAngle=compass.getAngle();
+    // turn on Arduino's LED to indicate we are in calibration mode
+
 }
 
 void loop() {
@@ -28,7 +34,6 @@ void loop() {
   int angle=compass.calibratedAngles(startingAngle,current);
   seekerInput=seeker.ReadAC();
     orient(angle);
-    //Serial.println(angle);
     while(!oriented){
       current=compass.getAngle();
       angle=compass.calibratedAngles(startingAngle,current);
@@ -37,92 +42,39 @@ void loop() {
     followball(seekerInput.Direction, seekerInput.Strength);
 }
 void followball(byte k,byte f){
-  //LEJOS
-  if(f<130){
-    //robot.on();
     switch(k){
       case 0:
-      if(!closeB)
-        robot.diagonalbackright(255);
-      else
-        robot.moveright(255);
+      robot.moveback(0);
       break;
       case 1:
       robot.moveback(255);
-      closeB=false;
       break;
       case 2:
       robot.moveback(255);
-      closeB=false;
       break;
       case 3:
-      robot.moveright(255);
-      closeB=false;
+      robot.moveright(SPEED);
       break;
       case 4:
-      robot.moveright(255);
-      closeB=false;
+      robot.moveright(SPEED);
       break;
       case 5:
-      robot.movefront(255);
+      robot.movefront(75);
       break;
       case 6:
-      robot.moveleft(255);
-      closeB=false;
+      robot.moveleft(SPEED);
       break;
       case 7:
-      robot.moveleft(255);
-      closeB=false;
+      robot.moveleft(SPEED);
       break;
       case 8:
       robot.moveback(255);
-      closeB=false;
       break;
       case 9:
       robot.moveback(255);
-      closeB=false;
       break;
       
     }
-  }
-  //CERCA
-  else{
-    //robot.sleep();
-  closeB=true;  
-  switch(k){
-      case 0:
-      robot.sleep();
-      break;
-      case 1:
-      robot.moveleft(255);
-      break;
-      case 2:
-      robot.moveright(150);
-      break;
-      case 3:
-      robot.moveright(120);
-      break;
-      case 4:
-      robot.moveright(50);
-      break;
-      case 5:
-      robot.movefront(255);
-      break;
-      case 6:
-      robot.moveleft(50);
-      break;
-      case 7:
-      robot.moveleft(120);
-      break;
-      case 8:
-      robot.moveleft(150);
-      break;
-      case 9:
-      robot.moveright(255);
-      break;
-      
-    }
-  }
 }
 void orient(int ang){
 if(ang>5&&ang<355){
@@ -130,17 +82,14 @@ oriented=false;
 if(ang<180&&ang>5){
   int vel=ang+50;
   robot.turnleft(vel);
-  //Serial.println(vel);
 }
 if(ang>=180&&ang<355){
   int vel=360-ang+50;
   robot.turnright(vel);
-  
-  //Serial.println(vel);
 }
 }
 else{
-  //Serial.println("nada!");
   oriented=true;
 }
 }
+
