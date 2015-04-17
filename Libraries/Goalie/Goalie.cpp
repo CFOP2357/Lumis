a319489a8e76c55d6790cd32047196f6d2d9ab13
xaviarazor <prietojavi551@gmail.com>
2015-04-16 21:10:31 -0500

    Update Goalie.cpp

--- Libraries/Goalie/Goalie.cpp
+++ Libraries/Goalie/Goalie.cpp
@@ -2,12 +2,7 @@
 #include "Arduino.h"
 #include "SPI.h"
 #include "Pixy.h"
-int Goalie::getEnemy(){
-	return enemy;
-}
-int Goalie::getSelf(){
-	return self;
-}
+
 void Goalie::setEnemy(int index){
 	enemy=index;
 }
@@ -20,12 +15,12 @@ void Goalie::setYellow(int index){
 void Goalie::setBlue(int index){
 	blue=index;
 }
-void Goalie::calibrate(){
+void Goalie::calibrate(Pixy send){
 	int area=0;
 	int area2=0;
-	pixelIn pixels=getInfo(yellow);
+	pixelIn pixels=getInfo(yellow,send);
 	area=pixels.area;
-	pixels=getInfo(blue);
+	pixels=getInfo(blue,send);
 	area2=pixels.area;
 	if(area>area2){
 		setEnemy(yellow);
@@ -36,14 +31,35 @@ void Goalie::calibrate(){
 		setSelf(yellow);
 	}
 }
-pixelIn Goalie::getInfo(int index){
-	Pixy pixy;
-	pixy.init();
-	pixelIn pixelInf;
-	pixelInf.x=pixy.blocks[index].x;
-	pixelInf.y=pixy.blocks[index].y;
-	pixelInf.height=pixy.blocks[index].height;
-	pixelInf.width=pixy.blocks[index].width;
-	pixelInf.area=pixelInf.height*pixelInf.width;
+pixelIn Goalie::getInfo(int index,Pixy ps){
+pixelIn pixelInf;
+static int i = 0;
+int j;
+uint16_t blocks;
+char buf[32];
+blocks = pixy.getBlocks();
+if (blocks){
+	i++;
+	if (i%50==0){
+			
+				pixelInf.x=ps.blocks[index].x;
+				pixelInf.y=ps.blocks[index].y;
+				pixelInf.height=ps.blocks[index].height;
+				pixelInf.width=ps.blocks[index].width;
+				pixelInf.area=pixelInf.height*pixelInf.width;
+
+			
+		}
+	}
 	return pixelInf;
+}
+pixelIn Goalie::getEnemy(Pixy px){
+	pixelIn enemyIn;
+	enemyIn=getInfo(enemy,px);
+	return enemyIn;
+}
+pixelIn Goalie::getSelf(Pixy px){
+	pixelIn selfIn;
+	selfIn=getInfo(self,px);
+	return selfIn;
 }
